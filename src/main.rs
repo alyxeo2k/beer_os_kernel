fbc9c5cb4b0a8fe86385c0d62cb355cc645e8402
MichaelLovesTea <mike.yermonsky@yahoo.com>
2022-11-20 02:55:27 -0600

    Made printing function and testing env and github repo

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[Ok]");
+    }
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+use core::panic::PanicInfo;
+//use core::fmt::Write;
+
+mod vga_buffer;
+mod serial;
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[Failed]\n");
+    serial_println!("ErrorL {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output")
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    //let mut write = vga_buffer::WRITER.lock();
+    println!("Hello World{}", "!");
+    println!("Test Math: {} {}", 22.22, 125/22);
+    //panic!("This is a test panic message");
+
+    #[cfg(test)]
+    test_main();
+    print!("...");
+
+    loop {}
+}
