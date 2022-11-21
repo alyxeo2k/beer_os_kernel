9321cf33bd3249efd3229c9fb1f1401883d5fb54
MichaelLovesTea <mike.yermonsky@yahoo.com>
2022-11-20 19:01:16 -0600

    added special case double page fault handler

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,17 +1,24 @@
 use x86_64::VirtAddr;
 use x86_64::structures::tss::TaskStateSegment;
-use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor};
+use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor, SegmentSelector};
 use lazy_static::lazy_static;
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
 lazy_static! {
-	static ref GDT: GlobalDescriptorTable = {
+	static ref GDT: (GlobalDescriptorTable, Selectors) = {
 		let mut gdt = GlobalDescriptorTable::new();
+		let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+		let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
 		gdt.add_entry(Descriptor::kernel_code_segment());
 		gdt.add_entry(Descriptor::tss_segment(&TSS));
-		gdt
+		(gdt, Selectors { code_selector, tss_selector })
 	};
+}
+
+struct Selectors {
+	code_selector: SegmentSelector,
+	tss_selector: SegmentSelector,
 }
 
 lazy_static! {
@@ -30,5 +37,12 @@ lazy_static! {
 }
 
 pub fn init() {
-	GDT.load();
+	use x86_64::instructions::tables::load_tss;
+	use x86_64::instructions::segmentation::{CS, Segment};
+
+	GDT.0.load();
+	unsafe {
+		CS::set_reg(GDT.1.code_selector);
+		load_tss(GDT.1.tss_selector);
+	}
 }
